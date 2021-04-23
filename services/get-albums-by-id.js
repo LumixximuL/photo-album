6b175fcf8329fea62cb1741a326d9e402a2af6f2
Taylor Webber <taylor.j.webber@gmail.com>
2021-04-23 12:34:51 -0500

    chore: remove unnecessary header

--- services/get-albums-by-id.js
+++ services/get-albums-by-id.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 export default async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/photos', {headers: {'Origin': '*'}});
+    const response = await fetch('https://jsonplaceholder.typicode.com/photos');
     const albumsById = {};
 
     if (response.ok) {
